fef025be5c926dc81e87901ad59f299fab1d0d30
bala-amypo <bala@amypo.in>
2025-12-20 14:54:24 +0000

    User commit

--- src/main/java/com/example/demo/util/JsonUtil.java
+++ src/main/java/com/example/demo/util/JsonUtil.java
@@ -1,17 +1,1 @@
 package com.example.demo.util;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public class JsonUtil {
-    private static final ObjectMapper mapper = new ObjectMapper();
-
-    public static String toJson(Object obj) {
-        try { return mapper.writeValueAsString(obj); }
-        catch (Exception e) { throw new RuntimeException(e); }
-    }
-
-    public static <T> T fromJson(String json, Class<T> clazz) {
-        try { return mapper.readValue(json, clazz); }
-        catch (Exception e) { throw new RuntimeException(e); }
-    }
-}
