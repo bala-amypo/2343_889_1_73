3d2cc193f795084ad97e0b8123bf16c0f0d45b6d
bala-amypo <bala@amypo.in>
2025-12-23 09:56:31 +0000

    User commit

--- src/main/java/com/example/demo/util/HibernateQueryUtil.java
+++ src/main/java/com/example/demo/util/HibernateQueryUtil.java
@@ -7,20 +7,24 @@ import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
 import org.springframework.stereotype.Component;
+
 import java.util.List;
 
 @Component
 public class HibernateQueryUtil {
+
     @PersistenceContext
     private EntityManager entityManager;
 
     public List<ApprovalAction> findActionsByApproverUsingCriteria(Long approverId) {
+
         CriteriaBuilder cb = entityManager.getCriteriaBuilder();
         CriteriaQuery<ApprovalAction> cq = cb.createQuery(ApprovalAction.class);
         Root<ApprovalAction> root = cq.from(ApprovalAction.class);
-        
-        cq.select(root).where(cb.equal(root.get("approverId"), approverId));
-        
+
+        cq.select(root)
+          .where(cb.equal(root.get("approverId"), approverId));
+
         return entityManager.createQuery(cq).getResultList();
     }
 }
