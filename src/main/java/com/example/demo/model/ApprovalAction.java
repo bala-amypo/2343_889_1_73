432f8fa1625a31fb4e35de495589e49e0a23b219
bala-amypo <bala@amypo.in>
2025-12-23 10:36:26 +0000

    User commit

--- src/main/java/com/example/demo/model/ApprovalAction.java
+++ src/main/java/com/example/demo/model/ApprovalAction.java
@@ -10,32 +10,39 @@ public class ApprovalAction {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    
+   
     @Column(name = "request_id", nullable = false)
     private Long requestId;
 
-    @Column(name = "level_number", nullable = false)
-    private Integer levelNumber;
+    
+    @Column(name = "level", nullable = false)
+    private Integer level;
+
+    
+    @Column(name = "approver_id", nullable = false)
+    private Long approverId;
 
     @Column(name = "action", nullable = false)
     private String action;
 
-    
+   
+
     public ApprovalAction() {
     }
 
-    public ApprovalAction(Long requestId, Integer levelNumber, String action) {
+    public ApprovalAction(Long requestId, Integer level, Long approverId, String action) {
         this.requestId = requestId;
-        this.levelNumber = levelNumber;
+        this.level = level;
+        this.approverId = approverId;
         this.action = action;
     }
 
-   
+    // ---------- Getters & Setters ----------
+
     public Long getId() {
         return id;
     }
 
-    
     public Long getRequestId() {
         return requestId;
     }
@@ -44,13 +51,22 @@ public class ApprovalAction {
         this.requestId = requestId;
     }
 
-   
-    public Integer getLevelNumber() {
-        return levelNumber;
+    
+    public Integer getLevel() {
+        return level;
     }
 
-    public void setLevelNumber(Integer levelNumber) {
-        this.levelNumber = levelNumber;
+    public void setLevel(Integer level) {
+        this.level = level;
+    }
+
+   
+    public Long getApproverId() {
+        return approverId;
+    }
+
+    public void setApproverId(Long approverId) {
+        this.approverId = approverId;
     }
 
     public String getAction() {
--- src/main/java/com/example/demo/repository/ApprovalActionRepository.java
+++ src/main/java/com/example/demo/repository/ApprovalActionRepository.java
@@ -2,10 +2,11 @@ package com.example.demo.repository;
 
 import com.example.demo.model.ApprovalAction;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 
 public interface ApprovalActionRepository extends JpaRepository<ApprovalAction, Long> {
 
-   
-    List<ApprovalAction> findByLevelNumberAndAction(Integer levelNumber, String action);
+  
+    List<ApprovalAction> findByLevelAndAction(Integer level, String action);
 }
